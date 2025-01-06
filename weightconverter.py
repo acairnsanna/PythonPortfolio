c1509564bc3f3997d98619406376ff4de8dd4b9a
acairnsanna <83887641+acairnsanna@users.noreply.github.com>
2025-01-05 19:42:36 -0500

    weightconverter.py

--- weightconverter.py
+++ weightconverter.py
@@ -1,8 +1,12 @@
+# define the variables and gather input from user
+
 weight = int(input('Weight: '))
 unit = input('(L)bs or (K)g: ')
 if unit.upper() == "L":
     converted = weight * 0.45
     print(f"You are {converted} kilos")
+    # converts from L to Kg
 else:
     converted = weight / 0.45
     print(f"You are {converted} pounds")
+    # converts from Kg to L
